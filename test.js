030b97bf4739790bbe9c536693ace420d6bcb9ea
Damian Senn <damian.senn@gmail.com>
2011-10-30 14:05:48 +0100

    fix test.js and add a picture to get the test actually working

--- test.js
+++ test.js
@@ -1,23 +1,23 @@
-#!/usr/local/bin/node
-var sys  = require('sys');
+#!/usr/bin/env node
+
+var util  = require('util');
 var fs   = require('fs');
 var path = require('path');
-var gd   = require('./gd');
+var gd   = require('./node-gd');
 
-var source = './funnyfog.png';
-var target = './funnyfog.thumb.png';
+var source = './circle.png';
+var target = './circle.thumb.png';
 
-if (path.exists(target)) fs.unlink(target);
+if (path.existsSync(target)) fs.unlinkSync(target);
 
 gd.openPng(
 	source,
-	function(png, path) {
-		if(png) {
-			var w = Math.floor(png.width/2), h = Math.floor(png.height/2);
-			var target_png = gd.createTrueColor(w, h);
+	function(err, png) {
 
-			png.copyResampled(target_png,0,0,0,0,w,h,png.width,png.height);
-			target_png.savePng(target, 1, gd.noop);
-		}
+		var w = Math.floor(png.width/2), h = Math.floor(png.height/2);
+		var target_png = gd.createTrueColor(w, h);
+
+		png.copyResampled(target_png,0,0,0,0,w,h,png.width,png.height);
+		target_png.savePng(target, 1, gd.noop);
 	}
 );
