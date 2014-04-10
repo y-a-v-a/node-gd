2bfbd4975eb4a118d2b65c6e0200ced121898630
Yun Lai <yun.l@impos.com.au>
2014-04-10 11:26:16 +1000

    Add grayscale method.

--- cpp/node-gd.cpp
+++ cpp/node-gd.cpp
@@ -328,9 +328,12 @@ protected:
       NODE_SET_PROTOTYPE_METHOD(t, "getTransparent", GetTransparent);
       NODE_SET_PROTOTYPE_METHOD(t, "colorDeallocate", ColorDeallocate);
       NODE_SET_PROTOTYPE_METHOD(t, "colorTransparent", ColorTransparent);
+      )
       // interlace
       t->PrototypeTemplate()->SetAccessor(String::NewSymbol("interlace"), InterlaceGetter, InterlaceSetter, Handle<Value>(), PROHIBITS_OVERWRITING);
       t->PrototypeTemplate()->SetAccessor(String::NewSymbol("colorsTotal"), ColorsTotalGetter, NULL, Handle<Value>(), PROHIBITS_OVERWRITING, ReadOnly);
+
+      NODE_SET_PROTOTYPE_METHOD(t, "toGrayscale",GrayScale);
 
       /**
        * Copying and Resizing Functions
@@ -1308,6 +1311,14 @@ protected:
 
       REQ_INT_ARG(0, color);
       gdImageColorTransparent(*im, color);
+
+      return args.This();
+    }
+
+    static Handle<Value> GrayScale(const Arguments &args)
+    {
+      Image *im = ObjectWrap::Unwrap<Image>(args.This());
+      gdImageGrayScale(*im);
 
       return args.This();
     }
