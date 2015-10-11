b87b6958f82242bc224f52cafc166f061e3e6e97
Vincent Bruijn <vebruijn@gmail.com>
2015-10-11 23:17:50 +0200

    fix error in build script

--- travis-linux-deps.sh
+++ travis-linux-deps.sh
@@ -2,5 +2,5 @@
 set -ev
 lsb_release -a
 
-sudo apt-get -qy yq --no-install-suggests --no-install-recommends --force-yes install libgd2-xpm-dev g++-4.8
+sudo apt-get -qy --no-install-suggests --no-install-recommends --force-yes install libgd2-xpm-dev g++-4.8
 npm install
