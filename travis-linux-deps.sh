7181b52383f235d6dfb0ae6f9ae82317e8ca26a4
Vincent Bruijn <vebruijn@gmail.com>
2015-10-11 23:38:41 +0200

    reset travis build

--- travis-linux-deps.sh
+++ travis-linux-deps.sh
@@ -2,6 +2,5 @@
 set -ev
 lsb_release -a
 
-sudo -E apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
-sudo -E apt-get -qy --no-install-suggests --no-install-recommends --force-yes install libgd2-xpm-dev g++-4.8
+sudo -E apt-get -qy --no-install-suggests --no-install-recommends --force-yes install libgd2-xpm-dev
 npm install
