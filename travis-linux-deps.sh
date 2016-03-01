1b4237fd67add55f4dd47399c4b5109738bc0fc4
Vincent Bruijn <vincent-bruijn@g-star.com>
2016-03-01 09:16:11 +0100

    revert the non-dev usage since gdlib-config is a dependency

--- travis-linux-deps.sh
+++ travis-linux-deps.sh
@@ -2,5 +2,5 @@
 set -ev
 lsb_release -a
 
-sudo -E apt-get -qy --no-install-suggests --no-install-recommends --force-yes install libgd2-xpm
+sudo -E apt-get -qy --no-install-suggests --no-install-recommends --force-yes install libgd2-xpm-dev
 npm install
